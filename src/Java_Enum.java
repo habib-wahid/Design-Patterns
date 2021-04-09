514e9dd964a86e989627967893c7b6f4bca1c25d
habib-wahid <habib11wahid@gmail.com>
2021-04-09 11:13:21 +0600

    java enum

---
+++ src/Java_Enum.java
@@ -0,0 +1,13 @@
+
+public class Java_Enum {
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level obj = Level.LOW;
+        System.out.println(obj);
+    }
+}
